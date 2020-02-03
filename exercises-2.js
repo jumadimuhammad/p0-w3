03cd35dacbed82e1080b484d277fb69aa6289517
jumadimuhammad <getjumadi@gmail.com>
2020-02-03 18:36:17 +0700

    upload task week 3

---
+++ exercises-2.js
@@ -0,0 +1,10 @@
+function balikString(str){
+    var res = ''
+    
+    for (var i = a.length-1; i >= 0; i--){
+        res += a[i]
+    }
+    return res
+}
+var a ='muhammad jumadi'
+console.log(balikString(a))
---
+++ exercises-3.js
@@ -0,0 +1,16 @@
+function dataHandling(a){
+    var res = []
+    for (var i = 0; i < input.length; i++)
+    {
+        console.log(input[i])
+        res += 'id no ' + input[[1][0]]
+        return res
+    }
+}
+
+var input = [
+    ['0001','muhammad jumadi'],
+    ['0002','muhammad ali']
+]
+
+console.log(dataHandling(input))
