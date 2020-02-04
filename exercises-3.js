77606a3e40bd7351a5c744138f23f5878ec7e27c
jumadimuhammad <getjumadi@gmail.com>
2020-02-04 09:35:30 +0700

    update exercises-3.js

--- exercises-3.js
+++ exercises-3.js
@@ -2,15 +2,16 @@ function dataHandling(a){
     var res = []
     for (var i = 0; i < input.length; i++)
     {
-        console.log(input[i])
-        res += 'id no ' + input[[1][0]]
-        return res
+        res += ' No id: ' + input[i][0]
+        res += ' Nama: ' + input[i][1]
+        res += ' Aalamat: ' + input[i][2]   
     }
+    return res
 }
 
 var input = [
-    ['0001','muhammad jumadi'],
-    ['0002','muhammad ali']
+    ['0001','muhammad jumadi','jakarta'],
+    ['0002','muhammad ali','jakarta']
 ]
 
 console.log(dataHandling(input))
